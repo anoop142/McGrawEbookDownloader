f1d91ace0973ae90c0972968c118cb4ba7f899f2
Anoop1507 <anoop.skumar1507@gmail.com>
2020-04-20 13:07:40 +0530

    Add files

---
+++ dl.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# Copyright Anoop S
+# McGraw hill express library ebook downloader
+import requests
+import argparse
+import subprocess
+import os
+from sys import argv
+import re
+import pypdftk
+# deafult download location
+output_dir = os.path.expanduser('~') + '/'
+# constants
+docpub = os.path.dirname(os.path.abspath(argv[0])) + '/bin/docpub'
+url_login = 'https://www.expresslibrary.mheducation.com/login'
+header = {
+    'user-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.62 Safari/537.36'}
+
+
+def downloader(dl_book_name, url_book_xod):
+    ebook_xod = output_dir + dl_book_name + ".xod"
+    ebook_pdf = output_dir + dl_book_name + ".pdf"
+    print(output_dir + dl_book_name)
+    print('Downloading..', end='\r')
+    # check for file
+    if os.path.isfile(ebook_pdf):
+        print("Already downloaded!")
+        quit()
+    elif os.path.isfile(ebook_xod) and not os.path.isfile(ebook_xod + ".aria2") and os.path.getsize(ebook_xod) != 0:
+        print("xod file found!")
+        return
+    else:
+        # download file
+        if not args.aria:
+            with open(ebook_xod, "wb") as f:
+                req = requests.get(url_book_xod, headers=header)
+                f.write(req.content)
+            print("Download complete!")
+        else:
+            os.chdir(output_dir)
+            subprocess.call(['aria2c', '-x16', url_book_xod,
+                             '-o', dl_book_name + ".xod"])
+
+
+def xod_to_pdf(dl_book_name):
+    print('Converting XOD to PDF...')
+    subprocess.call([docpub,
+                     '–f',
+                     'pdf',
+                     '-o',
+                     output_dir,
+                     output_dir + dl_book_name + ".xod"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
+
+
+def pdf_uncompress(pdf_wm_file, pdf_clean_file):
+    print('Decompressing..')
+    pypdftk.uncompress(pdf_wm_file, pdf_clean_file)
+
+
+def pdf_compress(pdf_wm_file, pdf_clean_file):
+    print('Compressing...')
+    pypdftk.compress(pdf_clean_file, pdf_wm_file)
+
+
+def strip_watermark(dl_book_name):
+    pdf_wm_file = output_dir + dl_book_name + ".pdf"
+    pdf_clean_file = output_dir + dl_book_name + "_clean.pdf"
+    pdf_uncompress(pdf_wm_file, pdf_clean_file)
+    with open(output_dir + dl_book_name + "_clean.pdf", "rb") as f:
+        data = f.read()
+        data = data.replace(b'www.pdftron.com', b'')
+    with open(output_dir + dl_book_name + "_clean.pdf", "wb") as out_file:
+        out_file.write(data)
+    pdf_compress(pdf_wm_file, pdf_clean_file)
+    # clean up
+    os.remove(pdf_clean_file)
+    os.remove(output_dir + dl_book_name + ".xod")
+    print('Finished!')
+
+
+def main(url_book, username, password):
+    login_params = {
+        'loginuser': username,
+        'loginpwd': password,
+        'task': 'login',
+        'login': 'Login'}
+    with requests.Session() as s:
+        req = s.post(url_login, headers=header, params=login_params)
+        req = s.get(url_book, headers=header)
+        regex = r'bookUrl:\s*?"(.+)"'
+        matches = re.findall(regex, req.text)
+        url_book_xod = matches[0]
+        regex = r'bookTitle :\s*?"(.+)"'
+        matches = re.findall(regex, req.text)
+        book_title = matches[0]
+        regex = r'authorName :\s*?"(.+)"'
+        matches = re.findall(regex, req.text)
+    book_author = matches[0]
+    dl_book_name = book_title + '_' + book_author
+    dl_book_name = re.sub("[^a-zA-Z0-9]+", "_", dl_book_name)
+    downloader(dl_book_name, url_book_xod)
+    xod_to_pdf(dl_book_name)
+    strip_watermark(dl_book_name)
+
+
+# CLI
+parser = argparse.ArgumentParser(
+    description='ebook-dl by Anoop. A script to download ebooks as pdf from express library')
+parser.add_argument(
+    'url',
+    help='ebook url',
+    action='store',
+    metavar='url')
+parser.add_argument(
+    '-u',
+    dest='username',
+    required=True,
+    metavar='username',
+    action='store')
+parser.add_argument(
+    '-p',
+    metavar='password',
+    required=True,
+    dest='password',
+    action='store')
+parser.add_argument(
+    '-x',
+    dest='aria',
+    help='external downloader aria(recommended -faster dl)',
+    action='store_true')
+parser.add_argument(
+    '-o',
+    metavar='path',
+    help='output dir',
+    dest='output_dir',
+    action='store')
+
+args = parser.parse_args()
+# make output dir proper
+if args.output_dir:
+    if args.output_dir.endswith(""):
+        output_dir = args.output_dir + "/"
+    else:
+        output_dir = args.output_dir
+# main
+main(args.url, args.username, args.password)
